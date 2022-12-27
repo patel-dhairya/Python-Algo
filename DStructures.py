f47ced05092287d1d6c11b8625f259bff0784356
patel-dhairya <p5d7@proton.me>
2022-12-27 20:24:18 +0530

    Update DStructures.py
    Added double linked list data structure

--- DStructures.py
+++ DStructures.py
@@ -41,9 +41,10 @@ class MyHeap:
         node = self.heap[l]
         size = len(self.heap)
         while True:
-            left_child_index = 2*l
+            left_child_index = 2 * l
             right_child_index = left_child_index + 1
-            if right_child_index < size and self.heap[right_child_index]<node and self.heap[right_child_index]<self.heap[left_child_index]:
+            if right_child_index < size and self.heap[right_child_index] < node and self.heap[right_child_index] < \
+                    self.heap[left_child_index]:
                 self.heap[l] = self.heap[right_child_index]
                 self.rank[self.heap[right_child_index]] = l
                 l = right_child_index
@@ -61,7 +62,66 @@ class MyHeap:
         del self.rank[old]
         self.heap[old_index] = new
         self.rank[new] = old_index
-        if old<new:
+        if old < new:
             self.down_heapify(old_index)
         else:
             self.up_heapify(old_index)
+
+
+#  ----------------------------------------------Double Linked List--------------------------------------------------
+class Node:
+    # Node class for double linked list data structure
+
+    def __init__(self, data):
+        """
+        Each node will be connected to other nodes with help of previous and next to complete double linked list
+        :param data: value stored in node
+        """
+        self.prev = None
+        self.data = data
+        self.next = None
+
+
+class DoubleLL:
+    def __init__(self):
+        "Each Dobule Linked list will have a head. If head is none then Linked List is empty"
+        self.head = None
+
+    def __len__(self) -> int:
+        "Return length of double linked list"
+        i = 0
+        temp = self.head
+        while temp is not None:
+            temp = temp.next
+            i += 1
+        return i
+
+    def is_empty(self) -> bool:
+        """
+        Check if Double linked list is empty or not
+        :return: Boolean
+        """
+        return False if self.head else True
+
+    def search(self, value) -> Node:
+        """
+        Search if given value exist as node in linked list
+        :param value: data of node to find
+        :return: Pointer to node with data = value
+        """
+        temp = self.head
+        while temp is not None and temp.data != value:
+            temp = temp.next
+        return temp
+
+    def prepend(self, new_node) -> None:
+        """
+        Insert new node in front of linked list
+        :param new_node: New node to insert
+        :return: None
+        """
+        new_node.next = self.head
+        new_node.prev = None
+        if self.head is not None:
+            self.head.prev = new_node
+        self.head = new_node
